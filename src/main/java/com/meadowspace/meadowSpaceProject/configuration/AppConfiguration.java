e3f3c297bfc191b5b7dbc8cf3c03f9402b188db8
Frankgmv <giovanyvelasquez2004@gmail.com>
2024-02-12 19:37:05 -0500

    add cors

---
+++ src/main/java/com/meadowspace/meadowSpaceProject/configuration/AppConfiguration.java
@@ -0,0 +1,22 @@
+package com.meadowspace.meadowSpaceProject.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@CrossOrigin
+public class AppConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000")
+			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.allowedHeaders("*");
+	}
+}
